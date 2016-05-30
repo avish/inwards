31e823f5fa71c9fbdb6b90b854343c7056e86fc1
Avishay Lavie <avishay.lavie@gmail.com>
2016-05-30 22:49:45 +0300

    VoomVoom: adjust parameters, use maximum intensity

--- src/voomvoom/voomvoom.ino
+++ src/voomvoom/voomvoom.ino
@@ -3,11 +3,13 @@
 #define NUM_STRIPS 30     // Number of strips we control
 #define TRAVEL_TIME 7000  // Time in millis for voomvoom wave to travel across all strips
 #define CYCLE_TIME 5000   // Time in millis between single voom ending and starting anew (can be less than TRAVEL_TIME for multiple concurrent waves)
+#define INTENSITY_DAMP  0 // Dampening of maximum intensity; default is 3072. A value of 0 Burns the board when left on too long.
+
 
 ColorCycler* cyclers[NUM_STRIPS];
 
 long CYCLE_DURATIONS[] = {
-  100, // Fade-in time
+  50,   // Fade-in time
   0,   // On time
   500, // Fade-out time
   CYCLE_TIME // Wait time before starting again
@@ -21,10 +23,11 @@ CRGB CYCLE_COLORS[] = {
 };
 
 #define CYCLER_OFFSET (TRAVEL_TIME / NUM_STRIPS)
-#define UPDATE_RESOLUTION 25  // Millis between updates
+#define UPDATE_RESOLUTION 0     // Millis between updates
 
 void setup() {
-  TlcLed.init();
+  TlcLed.init(INTENSITY_DAMP);
+  
   for (int i = 0; i < NUM_STRIPS; i++) {
     cyclers[i] = new ColorCycler(4, CYCLE_COLORS, CYCLE_DURATIONS);
     cyclers[i]->start(CYCLER_OFFSET * i);
@@ -39,3 +42,4 @@ void loop() {
   TlcLed.update();
   delay(UPDATE_RESOLUTION);
 }
+
